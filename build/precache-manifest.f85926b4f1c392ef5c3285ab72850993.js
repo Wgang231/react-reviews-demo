4341faa20f9cc3dbde173f9574c0244742cc4f2c
Wgang231 <103540449+Wgang231@users.noreply.github.com>
2022-11-10 00:37:55 +0100

    Add files via upload

---
+++ build/precache-manifest.f85926b4f1c392ef5c3285ab72850993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0b4e0ff45b66f5f919027c2d91acac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdf1adc52237f8fd7625",
+    "url": "/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf1adc52237f8fd7625",
+    "url": "/static/js/main.4b711a02.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
